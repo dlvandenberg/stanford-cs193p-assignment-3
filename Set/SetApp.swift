d6bb12345c1bdd23e6cabd70b35d4e75730075e1
Dennis van den Berg <dennis.vandenberg@nedap.com>
2023-05-01 17:24:41 +0200

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Dennis van den Berg on 01/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
